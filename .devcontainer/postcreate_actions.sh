ddf3df01516f717051a9709cc822a11503266fe1
Vincent Olde Scholtenhuis <voldescholtenhuis@ilionx.com>
2025-04-11 11:03:12 +0000

    docker commands verwijderd want geen docker geinstalleerd in de devcontainer

--- .devcontainer/postcreate_actions.sh
+++ .devcontainer/postcreate_actions.sh
@@ -5,6 +5,3 @@ echo "Node version = $(node --version)"
 echo "Npm version  = $(npm --version)"
 # update theme
 sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc
-# check of docker network npm_default bestaat. Nodig voor connect vanuit devcontainer met normale backend docker
-docker network inspect npm_default >/dev/null 2>&1 || docker network create -d bridge npm_default
-docker network list
