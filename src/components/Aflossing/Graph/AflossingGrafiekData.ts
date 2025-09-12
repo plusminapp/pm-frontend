9c9a754397638215a47dccbdcb4e6fafd8864004
Patrick Berkhout <patrickberkhout@users.noreply.github.com>
2025-09-12 14:21:55 +0200

    reformat

--- src/components/Aflossing/Graph/AflossingGrafiekData.ts
+++ src/components/Aflossing/Graph/AflossingGrafiekData.ts
@@ -1,5 +1,5 @@
-import dayjs from "dayjs";
-import { SaldoDTO } from "../../../model/Saldo";
+import dayjs from 'dayjs';
+import { SaldoDTO } from '../../../model/Saldo';
 
 type AflossingGrafiekData = {
   maand: string;
@@ -10,29 +10,40 @@ type AflossingGrafiekData = {
 type AflossingGrafiekDataMap = Record<string, Record<string, number>>;
 
 export function getData(aflossingSaldi: SaldoDTO[]): Record<string, any>[] {
-  const aflossingGrafiekDataLijst = aflossingSaldi.flatMap(genereerAflossingSaldi);
-  const aflossingGrafiekDataMap: AflossingGrafiekDataMap = aflossingGrafiekDataLijst.reduce((acc, item) => {
-    if (!acc[item.maand]) {
-      acc[item.maand] = {};
-    }
-    const key = item.rekeningNaam.replace(/\s/g, '').toLowerCase();
-    acc[item.maand][key] = item.bedrag;
-    return acc;
-  }, {} as AflossingGrafiekDataMap);
+  const aflossingGrafiekDataLijst = aflossingSaldi.flatMap(
+    genereerAflossingSaldi,
+  );
+  const aflossingGrafiekDataMap: AflossingGrafiekDataMap =
+    aflossingGrafiekDataLijst.reduce((acc, item) => {
+      if (!acc[item.maand]) {
+        acc[item.maand] = {};
+      }
+      const key = item.rekeningNaam.replace(/\s/g, '').toLowerCase();
+      acc[item.maand][key] = item.bedrag;
+      return acc;
+    }, {} as AflossingGrafiekDataMap);
 
-  const result: Record<string, any>[] = Object.entries(aflossingGrafiekDataMap).map(([maand, saldi]) => ({
+  const result: Record<string, any>[] = Object.entries(
+    aflossingGrafiekDataMap,
+  ).map(([maand, saldi]) => ({
     month: maand,
     ...saldi,
   }));
   return result;
 }
 
-const genereerAflossingSaldi = (aflossingSaldoDTO: SaldoDTO): AflossingGrafiekData[] => {
-  const formatter = "YYYY-MM";
+const genereerAflossingSaldi = (
+  aflossingSaldoDTO: SaldoDTO,
+): AflossingGrafiekData[] => {
+  const formatter = 'YYYY-MM';
   const aflossingGrafiekDataLijst: AflossingGrafiekData[] = [];
   let huidigeBedrag = Number(aflossingSaldoDTO.aflossing?.eindBedrag ?? 0);
-  let startAflossing = dayjs(aflossingSaldoDTO.aflossing?.startDatum).startOf("month");
-  let startPeilDatumPeriode = dayjs(aflossingSaldoDTO.budgetPeilDatum).subtract(1,'month').startOf("month")
+  let startAflossing = dayjs(aflossingSaldoDTO.aflossing?.startDatum).startOf(
+    'month',
+  );
+  let startPeilDatumPeriode = dayjs(aflossingSaldoDTO.budgetPeilDatum)
+    .subtract(1, 'month')
+    .startOf('month');
 
   let peilMaand = startAflossing;
 
@@ -42,11 +53,17 @@ const genereerAflossingSaldi = (aflossingSaldoDTO: SaldoDTO): AflossingGrafiekDa
       rekeningNaam: aflossingSaldoDTO.rekeningNaam,
       bedrag: huidigeBedrag,
     });
-    huidigeBedrag -=( aflossingSaldoDTO.budgetMaandBedrag ?? 0) + (aflossingSaldoDTO.achterstand ?? 0);
-    peilMaand = peilMaand.add(1, "month");
+    huidigeBedrag -=
+      (aflossingSaldoDTO.budgetMaandBedrag ?? 0) +
+      (aflossingSaldoDTO.achterstand ?? 0);
+    peilMaand = peilMaand.add(1, 'month');
   }
 
-  peilMaand = dayjs(startPeilDatumPeriode.isBefore(startAflossing) ? startAflossing : startPeilDatumPeriode);
+  peilMaand = dayjs(
+    startPeilDatumPeriode.isBefore(startAflossing)
+      ? startAflossing
+      : startPeilDatumPeriode,
+  );
   huidigeBedrag = -aflossingSaldoDTO.openingsBalansSaldo;
   aflossingGrafiekDataLijst.push({
     maand: peilMaand.format(formatter),
@@ -54,8 +71,8 @@ const genereerAflossingSaldi = (aflossingSaldoDTO: SaldoDTO): AflossingGrafiekDa
     bedrag: huidigeBedrag,
   });
 
-  peilMaand = peilMaand.add(1, "month");
-  huidigeBedrag -= (aflossingSaldoDTO.betaling ?? 0);
+  peilMaand = peilMaand.add(1, 'month');
+  huidigeBedrag -= aflossingSaldoDTO.betaling ?? 0;
 
   while (huidigeBedrag > 0) {
     aflossingGrafiekDataLijst.push({
@@ -63,21 +80,22 @@ const genereerAflossingSaldi = (aflossingSaldoDTO: SaldoDTO): AflossingGrafiekDa
       rekeningNaam: aflossingSaldoDTO.rekeningNaam,
       bedrag: huidigeBedrag,
     });
-    huidigeBedrag -=( aflossingSaldoDTO.budgetMaandBedrag ?? 0) + (aflossingSaldoDTO.achterstand ?? 0);
-    peilMaand = peilMaand.add(1, "month");
+    huidigeBedrag -=
+      (aflossingSaldoDTO.budgetMaandBedrag ?? 0) +
+      (aflossingSaldoDTO.achterstand ?? 0);
+    peilMaand = peilMaand.add(1, 'month');
   }
   return aflossingGrafiekDataLijst;
-}
-
+};
 
 export function getSeries(aflossingSaldi: SaldoDTO[]) {
   return aflossingSaldi.map((aflossing) => {
     return {
-      type: "line",
-      xKey: "month",
+      type: 'line',
+      xKey: 'month',
       yKey: aflossing.rekeningNaam.toLowerCase().replace(/\s/g, ''),
       yName: aflossing.rekeningNaam,
       stacked: false,
     };
-  })
+  });
 }
--- src/pages/Aflossen.tsx
+++ src/pages/Aflossen.tsx
@@ -1,104 +1,121 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import Box from '@mui/material/Box';
 
-import { useEffect, useState } from "react";
-import { useCustomContext } from "../context/CustomContext";
+import { useEffect, useState } from 'react';
+import { useCustomContext } from '../context/CustomContext';
 
-import { ArrowDropDownIcon } from "@mui/x-date-pickers";
-import AflossingTabel from "../components/Aflossing/AflossingTabel";
-import { PeriodeSelect } from "../components/Periode/PeriodeSelect";
-import { SaldoDTO } from "../model/Saldo";
-import { RekeningGroepSoort } from "../model/RekeningGroep";
-import { berekenRekeningGroepIcoon } from "../components/Stand/BerekenStandKleurEnTekst";
-import { AflossingenAfbouwGrafiek } from "../components/Aflossing/Graph/AflossingenAfbouwGrafiek";
-import { BetalingsSoort } from "../model/Betaling";
+import { ArrowDropDownIcon } from '@mui/x-date-pickers';
+import AflossingTabel from '../components/Aflossing/AflossingTabel';
+import { PeriodeSelect } from '../components/Periode/PeriodeSelect';
+import { SaldoDTO } from '../model/Saldo';
+import { RekeningGroepSoort } from '../model/RekeningGroep';
+import { berekenRekeningGroepIcoon } from '../components/Stand/BerekenStandKleurEnTekst';
+import { AflossingenAfbouwGrafiek } from '../components/Aflossing/Graph/AflossingenAfbouwGrafiek';
+import { BetalingsSoort } from '../model/Betaling';
 
 export default function Aflossen() {
-
-  const { actieveHulpvrager, gekozenPeriode, stand, rekeningGroepPerBetalingsSoort } = useCustomContext();
+  const {
+    actieveHulpvrager,
+    gekozenPeriode,
+    stand,
+    rekeningGroepPerBetalingsSoort,
+  } = useCustomContext();
 
   const aflossing = rekeningGroepPerBetalingsSoort
-    .filter(r => r.betalingsSoort === BetalingsSoort.aflossen)
-    .flatMap(r => r.rekeningGroepen)
-    .flatMap(r => r.rekeningen)
+    .filter((r) => r.betalingsSoort === BetalingsSoort.aflossen)
+    .flatMap((r) => r.rekeningGroepen)
+    .flatMap((r) => r.rekeningen);
 
-  const [aflossingSaldi, setAflossingSaldi] = useState<SaldoDTO[]>([])
+  const [aflossingSaldi, setAflossingSaldi] = useState<SaldoDTO[]>([]);
 
-    useEffect(() => {
-      stand && setAflossingSaldi(stand.resultaatOpDatum.
-        filter((r: SaldoDTO) => r.rekeningGroepSoort === RekeningGroepSoort.aflossing)
-        );
+  useEffect(() => {
+    stand &&
+      setAflossingSaldi(
+        stand.resultaatOpDatum.filter(
+          (r: SaldoDTO) =>
+            r.rekeningGroepSoort === RekeningGroepSoort.aflossing,
+        ),
+      );
   }, [stand]);
 
   return (
     <>
-      {aflossingSaldi.length === 0 &&
+      {aflossingSaldi.length === 0 && (
         <>
-          <Typography variant='h4'>Aflossen</Typography>
-          <Typography variant='body2'>{actieveHulpvrager?.bijnaam} heeft op {gekozenPeriode?.periodeStartDatum} geen aflossingen ingericht.</Typography>
+          <Typography variant="h4">Aflossen</Typography>
+          <Typography variant="body2">
+            {actieveHulpvrager?.bijnaam} heeft op{' '}
+            {gekozenPeriode?.periodeStartDatum} geen aflossingen ingericht.
+          </Typography>
           <PeriodeSelect isAflossing />
         </>
-
-      }
-      {aflossingSaldi.length > 0 &&
+      )}
+      {aflossingSaldi.length > 0 && (
         <>
-          <Typography variant='h4'>Aflossen</Typography>
-          <Grid container spacing={2} columns={{ xs: 1, md: 3 }} justifyContent="space-between">
+          <Typography variant="h4">Aflossen</Typography>
+          <Grid
+            container
+            spacing={2}
+            columns={{ xs: 1, md: 3 }}
+            justifyContent="space-between"
+          >
             <Grid size={1} alignItems="start">
               <PeriodeSelect isAflossing />
             </Grid>
           </Grid>
-          <Accordion
-            elevation={2}>
+          <Accordion elevation={2}>
             <AccordionSummary
               expandIcon={<ArrowDropDownIcon />}
-              aria-controls={"afbouwgrafiek"}
-              id={"afbouwgrafiek"}>
-              <Typography
-                sx={{ color: 'FFF' }}
-                component="span">
+              aria-controls={'afbouwgrafiek'}
+              id={'afbouwgrafiek'}
+            >
+              <Typography sx={{ color: 'FFF' }} component="span">
                 Verwachte afbouw van de schulden/aflossingen
               </Typography>
             </AccordionSummary>
-            <AccordionDetails sx={{ p: 0 }} >
+            <AccordionDetails sx={{ p: 0 }}>
               <AflossingenAfbouwGrafiek
-                aflossingen={aflossing} aflossingSaldi={aflossingSaldi} />
+                aflossingen={aflossing}
+                aflossingSaldi={aflossingSaldi}
+              />
             </AccordionDetails>
           </Accordion>
         </>
-      }
-      {aflossingSaldi.map(saldoDTO =>
-        <Accordion
-          key={saldoDTO.rekeningNaam}
-          elevation={2}>
+      )}
+      {aflossingSaldi.map((saldoDTO) => (
+        <Accordion key={saldoDTO.rekeningNaam} elevation={2}>
           <AccordionSummary
             expandIcon={<ArrowDropDownIcon />}
             aria-controls={saldoDTO.rekeningNaam}
-            id={saldoDTO.rekeningNaam}>
+            id={saldoDTO.rekeningNaam}
+          >
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               {berekenRekeningGroepIcoon(22, saldoDTO)}
               <Typography
                 sx={{ color: 'FFF', ml: 1, whiteSpace: 'nowrap' }}
                 component="span"
-                align="left">
+                align="left"
+              >
                 {saldoDTO.rekeningNaam}
               </Typography>
             </Box>
           </AccordionSummary>
-          <AccordionDetails sx={{ p: 0 }} >
-            <AflossingTabel
-              aflossingSaldo={saldoDTO}
-            />
+          <AccordionDetails sx={{ p: 0 }}>
+            <AflossingTabel aflossingSaldo={saldoDTO} />
           </AccordionDetails>
         </Accordion>
-      )}
+      ))}
       {/* aflossingSaldi: {JSON.stringify(aflossingSaldi)} */}
       {/*<hr />
       series: {JSON.stringify(getSeries(aflossingSaldi))}
       <hr />
       data: {JSON.stringify(getData(aflossingSaldi))} */}
     </>
-  )
+  );
 }
-
