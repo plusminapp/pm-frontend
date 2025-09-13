39bae96d6173ddff86e9faa917dac41264ecd6c9
Patrick Berkhout <patrickberkhout@users.noreply.github.com>
2025-09-13 08:37:20 +0200

    Fix/lint errors (#20)
    * reformat
    * fix lint errors

--- src/pages/Sparen.tsx
+++ src/pages/Sparen.tsx
@@ -1,104 +1,104 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import Box from '@mui/material/Box';
 
-import { useEffect, useState } from "react";
-import { useCustomContext } from "../context/CustomContext";
+import { useEffect, useState } from 'react';
+import { useCustomContext } from '../context/CustomContext';
 
-import { ArrowDropDownIcon } from "@mui/x-date-pickers";
-import SparenTabel from "../components/Sparen/SparenTabel";
-import { PeriodeSelect } from "../components/Periode/PeriodeSelect";
-import { SaldoDTO } from "../model/Saldo";
-import { RekeningGroepSoort } from "../model/RekeningGroep";
-import { berekenRekeningGroepIcoon } from "../components/Stand/BerekenStandKleurEnTekst";
+import { ArrowDropDownIcon } from '@mui/x-date-pickers';
+import SparenTabel from '../components/Sparen/SparenTabel';
+import { PeriodeSelect } from '../components/Periode/PeriodeSelect';
+import { SaldoDTO } from '../model/Saldo';
+import { RekeningGroepSoort } from '../model/RekeningGroep';
+import { berekenRekeningGroepIcoon } from '../components/Stand/BerekenStandKleurEnTekst';
 
 export default function Sparen() {
-
   const { actieveHulpvrager, gekozenPeriode, stand } = useCustomContext();
 
-  // const sparen = rekeningGroepPerBetalingsSoort
-  //   .filter(r => r.betalingsSoort === BetalingsSoort.sparen)
-  //   .flatMap(r => r.rekeningGroepen)
-  //   .flatMap(r => r.rekeningen)
-
-  //   console.log("sparen", sparen);
-
-  const [sparenSaldi, setSparenSaldi] = useState<SaldoDTO[]>([])
+  const [sparenSaldi, setSparenSaldi] = useState<SaldoDTO[]>([]);
 
   useEffect(() => {
-        stand && setSparenSaldi(stand.resultaatOpDatum.
-          filter((r: SaldoDTO) => r.rekeningGroepSoort === RekeningGroepSoort.spaarrekening)
-        );
+    if (stand) {
+      setSparenSaldi(
+        stand.resultaatOpDatum.filter(
+          (r: SaldoDTO) =>
+            r.rekeningGroepSoort === RekeningGroepSoort.spaarrekening,
+        ),
+      );
+    }
   }, [stand]);
 
   return (
     <>
-      {sparenSaldi.length === 0 &&
+      {sparenSaldi.length === 0 && (
         <>
-          <Typography variant='h4'>Sparen</Typography>
-          <Typography variant='body2'>{actieveHulpvrager?.bijnaam} heeft op {gekozenPeriode?.periodeStartDatum} geen spaartegoed ingericht.</Typography>
+          <Typography variant="h4">Sparen</Typography>
+          <Typography variant="body2">
+            {actieveHulpvrager?.bijnaam} heeft op{' '}
+            {gekozenPeriode?.periodeStartDatum} geen spaartegoed ingericht.
+          </Typography>
           <PeriodeSelect />
         </>
-
-      }
-      {sparenSaldi.length > 0 &&
+      )}
+      {sparenSaldi.length > 0 && (
         <>
-          <Typography variant='h4'>Sparen pagina</Typography>
-          <Grid container spacing={2} columns={{ xs: 1, md: 3 }} justifyContent="space-between">
+          <Typography variant="h4">Sparen pagina</Typography>
+          <Grid
+            container
+            spacing={2}
+            columns={{ xs: 1, md: 3 }}
+            justifyContent="space-between"
+          >
             <Grid size={1} alignItems="start">
               <PeriodeSelect />
             </Grid>
           </Grid>
-          <Accordion
-            elevation={2}>
+          <Accordion elevation={2}>
             <AccordionSummary
               expandIcon={<ArrowDropDownIcon />}
-              aria-controls={"afbouwgrafiek"}
-              id={"afbouwgrafiek"}>
-              <Typography
-                sx={{ color: 'FFF' }}
-                component="span">
+              aria-controls={'afbouwgrafiek'}
+              id={'afbouwgrafiek'}
+            >
+              <Typography sx={{ color: 'FFF' }} component="span">
                 Verwachte opbouw van het spaartegoed
               </Typography>
             </AccordionSummary>
-            {/* <AccordionDetails sx={{ p: 0 }} >
-              <SpaartegoedAfbouwGrafiek
-                spaartegoed={sparen} sparenSaldi={sparenSaldi} />
-            </AccordionDetails> */}
           </Accordion>
         </>
-      }
-      {sparenSaldi.map(saldoDTO =>
-        <Accordion
-          key={saldoDTO.rekeningNaam}
-          elevation={2}>
+      )}
+      {sparenSaldi.map((saldoDTO) => (
+        <Accordion key={saldoDTO.rekeningNaam} elevation={2}>
           <AccordionSummary
             expandIcon={<ArrowDropDownIcon />}
             aria-controls={saldoDTO.rekeningNaam}
-            id={saldoDTO.rekeningNaam}>
+            id={saldoDTO.rekeningNaam}
+          >
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               {berekenRekeningGroepIcoon(22, saldoDTO)}
               <Typography
                 sx={{ color: 'FFF', ml: 1, whiteSpace: 'nowrap' }}
                 component="span"
-                align="left">
+                align="left"
+              >
                 {saldoDTO.rekeningNaam}
               </Typography>
             </Box>
           </AccordionSummary>
-          <AccordionDetails sx={{ p: 0 }} >
-            <SparenTabel
-              sparenSaldo={saldoDTO}
-            />
+          <AccordionDetails sx={{ p: 0 }}>
+            <SparenTabel sparenSaldo={saldoDTO} />
           </AccordionDetails>
         </Accordion>
-      )}
+      ))}
       {/* sparenSaldi: {JSON.stringify(sparenSaldi)} */}
       {/* <hr />
       series: {JSON.stringify(getSeries(sparenSaldi))}
       <hr />
       data: {JSON.stringify(getData(sparenSaldi))} */}
     </>
-  )
+  );
 }
-
